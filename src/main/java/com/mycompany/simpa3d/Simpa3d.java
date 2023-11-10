f8e80f5660e15a4b42e00c3e20c7040a52cf8dce
William Cáceres <willycaceres2002@gmail.com>
2023-11-10 13:10:06 -0600

    first commit

---
+++ src/main/java/com/mycompany/simpa3d/Simpa3d.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.simpa3d;
+
+/**
+ *
+ * @author dell
+ */
+public class Simpa3d {
+
+    public static void main(String[] args) {
+        new View().setVisible(true);
+    }
+}
